7f0a2e277b802714dcb3149474a126a94f29ca98
kerleyd <kerleyd@uk.ibm.com>
2017-07-18 16:32:14 +0100

    First commit

---
+++ pnget.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for model in `cat models`
+do
+
+
+	for i in `cat $model`
+ 		do
+		echo $model
+ 		lynx -dump -nolist "http://partsurfer.hpe.com/Search.aspx?type=PROD&SearchText=$i" | awk '/[0-9]{6}-[0-9]{3}/ || / [A-Z,0-9]{6} /' | tee -a  $model.file
+ 		done
+
+
+done
+
