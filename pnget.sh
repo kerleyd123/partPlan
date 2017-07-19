21835f5071893a664b96b93b639cb70ec5d1ae0f
kerleyd <kerleyd@uk.ibm.com>
2017-07-19 13:57:08 +0100

    Change single lines to multline

--- pnget.sh
+++ pnget.sh
@@ -7,7 +7,9 @@ do
 	for i in `cat $model`
  		do
 		echo $model
- 		lynx -dump -nolist "http://partsurfer.hpe.com/Search.aspx?type=PROD&SearchText=$i" | awk '/[0-9]{6}-[0-9]{3}/ || / [A-Z,0-9]{6} /' | tee -a  $model.file
+ 		lynx -dump -nolist "http://partsurfer.hpe.com/Search.aspx?type=PROD&SearchText=$i" | 
+ 				awk '/[0-9]{6}-[0-9]{3}/ || / [A-Z,0-9]{6} /' | 
+ 					tee -a  $model.file
  		done
 
 
