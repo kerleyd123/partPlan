d5d495fd34a9ea2b60f956fb14bfd49df10ffca7
kerleyd <kerleyd@uk.ibm.com>
2017-07-19 17:00:33 +0100

    First version

--- pnfmt.sh
+++ pnfmt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for model in `cat models`
+for model in `cat data/models`
 do
-     cat $model.file |	sed -e 's/^   //g' -e 's/\s/\t/' -re '/^[0-9A-Z]{6}-[0-9]{3}|^[0-9A-Z]{6}/!d'  | 
+     cat data/$model.file |	sed -e 's/^   //g' -e 's/\s/\t/' -re '/^[0-9A-Z]{6}-[0-9]{3}|^[0-9A-Z]{6}/!d'  | 
      		sort -k2  | 
      			 	uniq | 
-     			 		 awk 'BEGIN{OFS=FS="\t"} {$2=substr($2,1,34)}1' > $model.fmt
+     			 		 awk 'BEGIN{OFS=FS="\t"} {$2=substr($2,1,34)}1' > data/$model.fmt
 done
--- pnget.sh
+++ pnget.sh
@@ -2,16 +2,16 @@
 # A line to test a simple change
 # another my_brach line
 # anoher new newbrach line 
-for model in `cat models`
+for model in `cat data/models`
 do
 
 
-	for i in `cat $model`
+	for i in `cat data/$model`
  		do
 		echo $model
  		lynx -dump -nolist "http://partsurfer.hpe.com/Search.aspx?type=PROD&SearchText=$i" | 
  				awk '/[0-9]{6}-[0-9]{3}/ || / [A-Z,0-9]{6} /' | 
- 					tee -a  $model.file
+ 					tee -a  data/$model.file
  		done
 
 
